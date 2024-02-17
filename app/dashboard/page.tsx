20d65644ac695e04628fb03328097a93bb11838b
glody007 <glodymbutwile@gmail.com>
2024-02-17 20:14:49 +0200

    feat: time spent overview

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,49 +1,69 @@
+import { Metadata } from "next"
 import { OverviewCard } from "@/components/dashboard/overview-card";
 import { DashboardShell } from "@/components/dashboard/shell";
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { TimeSpent } from "@/components/dashboard/time-spent";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Analytics of the blog",
+}
 
 export default function DashboardPage() {
     return (
       <DashboardShell title="Dashboard" className="h-full pt-4">
         <div className="h-full grid md:grid-cols-[1fr_300px] gap-4">
-            <div className="flex flex-col gap-4">
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-                <div>
-                  <OverviewCard 
-                    title="Users" 
-                    description="450" 
-                    icon="user" 
-                  />
-                </div>
-                <div>
-                  <OverviewCard 
-                    title="Articles" 
-                    description="40" 
-                    icon="file" 
-                  />
-                </div>
-                <div>
-                  <OverviewCard 
-                    title="Likes" 
-                    description="60" 
-                    icon="heart" 
-                  />
-                </div>
-                <div>
-                  <OverviewCard 
-                    title="Comments" 
-                    description="79" 
-                    icon="message" 
-                  />
-                </div>
+          <div className="flex flex-col gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+              <div>
+                <OverviewCard 
+                  title="Users" 
+                  description="450" 
+                  icon="user" 
+                />
+              </div>
+              <div>
+                <OverviewCard 
+                  title="Articles" 
+                  description="40" 
+                  icon="file" 
+                />
+              </div>
+              <div>
+                <OverviewCard 
+                  title="Likes" 
+                  description="60" 
+                  icon="heart" 
+                />
+              </div>
+              <div>
+                <OverviewCard 
+                  title="Comments" 
+                  description="79" 
+                  icon="message" 
+                />
               </div>
             </div>
-            <Card>
+            <Card className="flex-1">
               <CardHeader>
-                <CardTitle>Activity tracking</CardTitle>
-                <CardDescription>Likes, Comments, Users</CardDescription>
+                <CardTitle>
+                  Time spent by users
+                </CardTitle>
+                <CardDescription>
+                  How many times user spent reading articles
+                </CardDescription>
               </CardHeader>
+              <CardContent>
+                <TimeSpent />
+              </CardContent>
             </Card>
+          </div>
+          <Card>
+            <CardHeader>
+              <CardTitle>Authors</CardTitle>
+              <CardDescription>Authors details</CardDescription>
+            </CardHeader>
+          </Card>
         </div>
       </DashboardShell>
     );
