4585f86f0c836f5bbbbff1ab5f94d49a38387c44
glody007 <glodymbutwile@gmail.com>
2024-02-19 14:56:29 +0200

    style: fix gap and text size

--- components/blog/article-actions.tsx
+++ components/blog/article-actions.tsx
@@ -18,15 +18,15 @@ export async function ArticleActions({ id }: Props) {
     const interactions = await getArticleInteractions(id)
     
     return (
-        <div className="mr-auto flex items-start gap-6 text-xs text-muted-foreground font-light px-3 py-2 rounded-2xl bg-muted">
-            <div className="flex items-center gap-1">
+        <div className="mr-auto flex items-start gap-6 text-sm text-muted-foreground font-light px-3 py-2 rounded-2xl bg-muted">
+            <div className="flex items-center gap-2">
                 <LikeButton articleId={id} />
                 <LikeList articleId={id}> 
                     <p>{interactions.comments.length}</p>
                 </LikeList>
             </div>
             <Comment articleId={id}>
-                <div className="flex items-center gap-1">
+                <div className="flex items-center gap-2">
                     <Icons.message className="w-5 h-5" />
                     <p>{interactions.likes.length}</p>
                 </div>
