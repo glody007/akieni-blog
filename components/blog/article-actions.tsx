010f475469a7f98b44d37a535fa9e8c4572241f8
glody007 <glodymbutwile@gmail.com>
2024-02-19 07:05:53 +0200

    style: actions

--- components/blog/article-actions.tsx
+++ components/blog/article-actions.tsx
@@ -9,7 +9,7 @@ export async function ArticleActions({ id }: Props) {
     const interactions = await getArticleInteractions(id)
     
     return (
-        <div className="flex items-center gap-6 text-sm text-muted-foreground font-light">
+        <div className="flex items-start gap-6 text-xs text-muted-foreground font-light">
             <div className="flex items-center gap-1">
                 <Icons.heart className="w-5 h-5" />
                 <p>{interactions.comments.length}</p>
