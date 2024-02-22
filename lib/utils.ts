ba77c2edc2302df9df4c2a43954dd065476a089b
glody007 <glodymbutwile@gmail.com>
2024-02-22 18:34:16 +0200

    feat: encode uri

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,5 +7,5 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function generateAfterAuthRedirectionLink(link: string) {
-  return `${env.NEXT_PUBLIC_SIGN_IN_PAGE}?redirect_url=${link}`
+  return `${env.NEXT_PUBLIC_SIGN_IN_PAGE}?redirect_url=${encodeURI(link)}`
 }
